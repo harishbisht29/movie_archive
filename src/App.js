cbf616c29615a80117a857fd1c7678e217d4d19b
harishbisht29 <harishbisht29@gmail.com>
2019-10-15 03:21:04 +0530

    Adding Trendy Movie Thumbnail Theme

--- src/App.js
+++ src/App.js
@@ -39,22 +39,39 @@ class App extends Component {
     else{
       data = []
     }
+
     return (
       <div className="container">
       <div className="jumbotron">
         <h1 className="display-4">Trendy Movies</h1>
       </div>
-      {data.map((movie) => (
-          <div className="card" key={movie.id}>
-            <div className="card-header">
-              #{movie.id} {movie.title}
-            </div>
-            <div className="card-body">
-              <p className="card-text">{movie.overview}</p>
-            </div>
-          </div>
+      <div className="album py-5 bg-light">
+        <div className="container">
+          <div className="row">
+          
+          {data.map((movie) => (
+                  <div className="col-md-4">
+                  <div className="card mb-4 box-shadow"  key={movie.id}>
+                    <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
+                     alt="Thumbnail [100%x225]" styles="{{height: 225px; width: 100%; display: block;}}"
+                     src={'https://image.tmdb.org/t/p/w500'+ movie.poster_path} data-holder-rendered="true" />
+                    <div className="card-body">
+                      <p className="card-text">{movie.overview}</p>
+                      <div className="d-flex justify-content-between align-items-center">
+                        <small className="text-muted">{movie.release_date}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
         ))}
-      
+ 
+ 
+
+
+          </div>
+        </div>
+      </div>
+           
     </div>
     );
   }
