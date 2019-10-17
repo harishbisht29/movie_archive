cc33b44c725031b31417d5600bd998d9e31d0847
harishbisht29 <harishbisht29@gmail.com>
2019-10-17 09:50:48 +0530

    Adding search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import Movielist from './Movielist';
 
 class App extends Component {
 
@@ -9,12 +10,24 @@ class App extends Component {
     this.state = {
       data: [],
       search_parameter: "",
-    
+      header:""
     }
+    this.handleClick = this.handleClick.bind(this);
+    this.setSearchField = this.setSearchField.bind(this);
   }
 
-  getMovieDetails(){
-    console.log("hi")
+  handleClick(){
+    console.log("Getting Movie Details")
+    var api_url = "https://api.themoviedb.org/3/search/multi?api_key=10e9e0a1fe452c0635e71c72e8f77d92&language=en-US&query=" + this.state.search_parameter + "&page=1&include_adult=false"
+    fetch(api_url)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          data: json,
+          search_parameter: this.state.search_parameter,
+          header:this.state.search_parameter
+        })
+      })
   }
   componentDidMount() {
      fetch('https://api.themoviedb.org/3/discover/movie?api_key=10e9e0a1fe452c0635e71c72e8f77d92&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
@@ -22,58 +35,63 @@ class App extends Component {
       .then(json => {
         this.setState({
           data: json,
-          search_parameter: "popular"
+          search_parameter: "popular",
+          header:"Popular Movies"
         })
       })
   }
+
+  setSearchField(value){
+    console.log(value)
+    this.setState({search_parameter: value })
+  }
   render() {
 
-    var { data, search_parameter } = this.state
+    var { data, search_parameter, header } = this.state
 
-    
-
-    if(search_parameter !== ""){
+    if(header !== ""){
       data = data.results
-      console.log(data)
+      // console.log(data)
     }
     else{
-      data = []
+      data = ""
     }
 
-    return (
-      <div className="container">
-      <div className="jumbotron">
-        <h1 className="display-4">Trendy Movies</h1>
-      </div>
-      <div className="album py-5 bg-light">
-        <div className="container">
-          <div className="row">
-          
-          {data.map((movie) => (
-                  <div className="col-md-4">
-                  <div className="card mb-4 box-shadow"  key={movie.id}>
-                    <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
-                     alt="Thumbnail [100%x225]" styles="{{height: 225px; width: 100%; display: block;}}"
-                     src={'https://image.tmdb.org/t/p/w500'+ movie.poster_path} data-holder-rendered="true" />
-                    <div className="card-body">
-                      <p className="card-text">{movie.overview}</p>
-                      <div className="d-flex justify-content-between align-items-center">
-                        <small className="text-muted">{movie.release_date}</small>
-                      </div>
+    if(data  !== "" ){
+      return (     
+      <div>
+     	<div className="row justify-content-center">
+                        <div className="col-12 col-md-10 col-lg-8">
+                            
+                                <div className="card-body row no-gutters align-items-center">
+                                    <div className="col-auto">
+                                        <i className="fas fa-search h4 text-body"></i>
+                                    </div>
+                                    
+                                    <div className="col">
+                                        <input onChange={event => this.setSearchField(event.target.value)} className="form-control form-control-lg form-control-borderless" type="text" placeholder="Search topics or keywords" />
+                                    </div>
+                                    
+                                    <div className="col-auto">
+                                        <button className="btn btn-lg btn-success" onClick={this.handleClick}>Search</button>
+                                    </div>
+                                    
+                                </div>
+                            
+                        </div>
+
                     </div>
-                  </div>
-                </div>
-        ))}
- 
- 
-
-
-          </div>
-        </div>
+        <Movielist movies={this.state.data} header={this.state.header} />  
       </div>
-           
-    </div>
-    );
+      );
+    }
+    else{
+      return(
+        <h1>Loading Data</h1>
+      )
+    }
+    
+  
   }
 }
 
---
+++ src/Movielist.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class Movielist extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    console.log("rendering Movielist")
+    console.log(this.props.movies.results)
+    
+    return (
+      <div className="container">
+      <div className="jumbotron mt-4">
+        <h1 className="display-4">{this.props.header}</h1>
+      </div>
+      <div className="album py-5 bg-light">
+        <div className="container">
+          <div className="row">
+          
+          {this.props.movies.results.map((movie) => (
+                  <div className="col-md-4">
+                  <div className="card mb-4 box-shadow"  key={movie.id}>
+                    <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
+                     alt="Thumbnail [100%x225]" styles="{{height: 225px; width: 100%; display: block;}}"
+                     src={'https://image.tmdb.org/t/p/w500'+ movie.poster_path} data-holder-rendered="true" />
+                    <div className="card-body">
+                      <p className="card-text">{movie.overview}</p>
+                      <div className="d-flex justify-content-between align-items-center">
+                        <small className="text-muted">{movie.release_date}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+        ))}
+ 
+ 
+
+
+          </div>
+        </div>
+      </div>
+           
+    </div>
+      );
+  }
+}
+
+export default Movielist;
